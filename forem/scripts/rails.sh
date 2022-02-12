d3b254c171610c3f179026355bef8a73ceb1245d
drunkod <drunkod@gmail.com>
2022-02-12 15:41:11 +0500

    dfv

--- forem/scripts/rails.sh
+++ forem/scripts/rails.sh
@@ -3,14 +3,21 @@ set -e
 
 source scripts/services.env
 
-# echo "Running db:setup"
-#SEEDS_MULTIPLIER=1 bin/rails db:seed
-#bin/rails  db:migrate
 
 echo "Running app_initializer:setup"
 until bin/rails app_initializer:setup
 do
 	echo "waiting before retrying app intialization"
+	sleep 300
+done
+
+# echo "Running db:setup"
+#SEEDS_MULTIPLIER=1 bin/rails db:seed
+#bin/rails  db:migrate
+echo "Running db:seed"
+until bin/rails db:seed
+do
+	echo "waiting before retrying db:seed"
 	sleep 300
 done
 
