8e565c61f95d9ae4a7473e3b7b245845c3591a7b
drunkod <drunkod@gmail.com>
2022-02-12 14:50:11 +0500

    cscf

--- forem/scripts/sidekiq.sh
+++ forem/scripts/sidekiq.sh
@@ -3,7 +3,7 @@ set -e
 
 source scripts/services.env
 
-while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' http://10.6.2.153:3000)" != "200" ]]
+while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' http://0.0.0.0:3000)" != "200" ]]
 do
 	echo "waiting for Rails application"
 	sleep 10
